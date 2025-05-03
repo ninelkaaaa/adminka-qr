a02a42900412c6c45e3edfbf2507a1463c14fc6b
fatihtekik <123174607+fatihtekik@users.noreply.github.com>
2025-05-03 15:58:36 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from services import db
+from routers import api_blueprint
+from flask_cors import CORS  
+from flask_migrate import Migrate
+app = Flask(__name__)
+migrate = Migrate(app, db)
+CORS(app)  
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aituproject_user:b1KUlQGvxriUeBBnX3CMWGPeEcBRRziy@dpg-cvrk5g8gjchc73bbksq0-a/aituproject'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+app.register_blueprint(api_blueprint)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
